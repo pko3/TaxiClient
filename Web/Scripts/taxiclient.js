91657220b7ac5452fa5b4eff02b4954e0388d710
MarianHorniak <marian.horniak@gmail.com>
2013-11-12 13:30:36 +0100

    mhp zmeny ver unia

--- Web/Scripts/taxiclient.js
+++ Web/Scripts/taxiclient.js
@@ -3,12 +3,13 @@
     ServiceUrl: "http://62.168.68.178/taxidFlexTest",
     Companies: {
         Version: 2,
-        Items:[
+        Items:[  
         { GUID_sysCompany: "2a6e8bd1-f425-4758-821c-de49f89d97e2", Title: "dFlex Nitra", Town: "Nitra" },
         { GUID_sysCompany: "2a6e8bd1-f425-4758-821c-de49f89d97e2", Title:"dFlex Nitra", Town:"Bratislava" },
         { GUID_sysCompany: "0dba79c0-d961-42ed-be24-684258890f99", Title:"CD Taxi", Town: "Nitra" },
         { GUID_sysCompany: "0dba79c0-d961-42ed-be24-684258890f99", Title:"CD Taxi", Town: "Levice" },
         { GUID_sysCompany: "0acffe88-27c2-4ec6-8ea6-0e311a3c1ad2", Title:"dflex Test", Town: "Bratislava" },
-        { GUID_sysCompany: "0acffe88-27c2-4ec6-8ea6-0e311a3c1ad2", Title:"dflex Test", Town: "Nitra" }
+        { GUID_sysCompany: "0acffe88-27c2-4ec6-8ea6-0e311a3c1ad2", Title: "dflex Test", Town: "Nitra" },
+        { GUID_sysCompany: "2a6e8bd1-f425-4758-821c-de49f89d97e4", Title: "City taxi Brno", Town: "Brno"}
         ]}
 }
